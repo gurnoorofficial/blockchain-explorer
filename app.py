c78e83250b2c87132d188f0d34ca239bc65b8fc6
Gurnoor Singh <gurnoorofficial@icloud.com>
2025-06-21 01:01:08 +0000

    VPS auto-push on Sat Jun 21 01:01:08 UTC 2025

--- app.py
+++ app.py
@@ -94,6 +94,7 @@ def verify_chain():
 
     return jsonify({"errors": errors, "messages": messages})
 
-if __name__ == "__main__":
-    app.run(host="209.38.87.101", port=5000, debug=True)
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=5000, debug=True)
+
 
